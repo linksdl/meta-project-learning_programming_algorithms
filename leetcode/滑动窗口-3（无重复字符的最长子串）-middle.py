82e1d037d09abf4fdd1a9ad3e7a07af7e8693d37
ShengDaolin <15900859362@163.com>
2021-09-12 15:05:43 +0100

    slide window

---
+++ leetcode/滑动窗口-3（无重复字符的最长子串）-middle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time        : 2021/9/12 2:51 下午
+# @Author      : linksdl
+# @ProjectName : futuretec-project-algorithm_leetcode
+# @File        : 滑动窗口-3（无重复字符的最长子串）-middle.py
+
+'''
+3. 无重复字符的最长子串
+https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+'''
+
+
+class Solution:
+    def lengthOfLonestSubstring(self, s):
+        n = len(s)
+        occ = set()
+        rk, ans = -1, 0
+        for i in range(n):
+            if i != 0:
+                occ.remove(s[i - 1])
+            while rk + 1 < n and s[rk + 1] not in occ:
+                occ.add(s[rk + 1])
+                rk += 1
+            ans = max(ans, rk - i + 1)
+        return ans
