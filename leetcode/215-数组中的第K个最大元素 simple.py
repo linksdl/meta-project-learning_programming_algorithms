970bd031d274a83a78a56db3d5d623a595990ada
ShengDaolin <15900859362@163.com>
2021-09-15 17:28:46 +0100

    selection sort algorithms

--- leetcode/215-数组中的第K个最大元素 simple.py
+++ leetcode/215-数组中的第K个最大元素 simple.py
@@ -11,6 +11,8 @@
 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
 https://leetcode-cn.com/leetbook/read/sort-algorithms/osxtdc/
+输入: [3,2,1,5,6,4] 和 k = 2
+输出: 5
 '''
 
 class Solution:
