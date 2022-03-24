fa791ab5ae02d7818a49594353b86dff763e15d2
Daolin Sheng <15900859362@163.com>
2022-03-24 16:06:09 +0800

    华为机考算法题-入门

---
+++ huawei/0-入门/HJ58-minKInteger.py
@@ -0,0 +1,36 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/3/24 15:36
+@File        : HJ58-minKInteger.py
+"""
+
+
+def minKInteger(n, k, nums):
+    """
+    描述
+    输入n个整数，找出其中最小的k个整数并按升序输出
+    本题有多组输入样例
+    数据范围：1 \le n \le 1000 \1≤n≤1000  ，输入的整数满足 1 \le val \le 10000 \1≤val≤10000
+    输入描述：
+    第一行输入两个整数n和k
+    第二行输入一个整数数组
+    输出描述：
+    从小到大输出最小的k个整数，用空格分开。
+    :param n:
+    :param k:
+    :param nums:
+    """
+    nums.sort()
+    if k >= n:
+        return ' '.join(nums)
+
+    else:
+        new_nums = nums[0:k]
+        # print(new_nums)
+        return ' '.join([str(s) for s in new_nums])
+
+
+print(minKInteger(5, 2, [1,3,5,7,2]))
+
+
