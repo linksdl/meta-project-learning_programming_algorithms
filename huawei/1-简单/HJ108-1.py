fa24a52b9615efaea48dbe271794a70e071e12fd
Daolin Sheng <15900859362@163.com>
2022-03-25 17:31:54 +0800

    华为机考算法题-容易

---
+++ huawei/1-简单/HJ108-1.py
@@ -0,0 +1,21 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/3/25 15:59
+@File        : HJ108-.py
+"""
+
+
+# HJ108 求最小公倍数
+
+
+def lcm(a, b):
+    for i in range(min(a, b), 0, -1):
+        if a % i == 0 and b % i == 0:
+            return a * b // i
+
+
+print(lcm(4, 8))
+
+
+
