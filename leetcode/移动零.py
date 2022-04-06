195f3c780993761dc963b6640e2618ded8de8f0a
Daolin Sheng <15900859362@163.com>
2022-04-07 00:50:41 +0800

    Leetcode-数组

---
+++ leetcode/移动零.py
@@ -0,0 +1,21 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/4/6 21:23
+@File        : 移动零.py
+"""
+
+# 移动零
+def moveZeroes(nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    n = len(nums)
+    left = right = 0
+    while right < n:
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+        right += 1
+
+
---
+++ 华为机考OD/排列组合.py
@@ -0,0 +1,8 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/4/5 16:55
+@File        : 排列组合.py
+"""
+
+
