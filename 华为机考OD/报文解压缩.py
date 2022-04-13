36abf1105b84ea93acd9e9e8b688029d2c5d46bf
Daolin Sheng <15900859362@163.com>
2022-04-13 21:23:10 +0800

    Huawei OD test

---
+++ 华为机考OD/报文解压缩.py
@@ -0,0 +1,40 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/4/11 17:18
+@File        : 报文解压缩.py
+"""
+
+
+def unzip(str):
+
+    mystack_char = []
+    mystack_num  = []
+    ans = ''
+    for s in str:
+        if s == '[':
+            mystack_char.append(s)
+
+        elif s.isdigit():
+            mystack_num.append(s)
+
+        elif s.isalpha():
+            mystack_char.append(s)
+
+        elif s == ']':
+            cc = []
+            while mystack_char[-1] != '[':
+                cc.append(mystack_char.pop())
+            n = int(mystack_num.pop())
+            ccc = ''.join(cc[::-1])
+            # print(ccc)
+            ccc *= n
+
+            mystack_char.pop() # 弹出 '['
+            mystack_char.append(ccc)
+            print(ccc)
+
+
+s = '3[m2[c]]'
+s = '3[k]2[mn]'
+unzip(s)
---
+++ 华为机考OD/矩形相交的面积.py
@@ -0,0 +1,51 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/4/11 17:46
+@File        : 矩形相交的面积.py
+"""
+
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a1 = input.nextInt();
+        int a2 = input.nextInt();
+        int a3 = input.nextInt();
+        int a4 = input.nextInt();
+        int b1 = input.nextInt();
+        int b2 = input.nextInt();
+        int b3 = input.nextInt();
+        int b4 = input.nextInt();
+        int c1 = input.nextInt();
+        int c2 = input.nextInt();
+        int c3 = input.nextInt();
+        int c4 = input.nextInt();
+        int[] a = {a1, a2, a3, a4};
+        int[] b = {b1, b2, b3, b4};
+        int[] c = {c1, c2, c3, c4};
+
+        int[] m = commonSpace(a, b);
+        int[] commonSpace = commonSpace(m, c);
+        System.out.println(commonSpace[2] * commonSpace[3]);
+
+    }
+
+    public static int[] commonSpace(int[] a, int[] b){
+        int x11 = a[0];
+        int y11 = a[1];
+        int x12 = a[0]+a[2];
+        int y12 = a[1]-a[3];
+
+        int x21 = b[0];
+        int y21 = b[1];
+        int x22 = b[0]+b[2];
+        int y22 = b[1]-b[3];
+
+        int xx = Math.max(x11,x21);
+        int yy = Math.min(y11,y21);
+
+        int x = Math.min(x12,x22);
+        int y = Math.max(y12,y22);
+
+        return new int[]{xx,yy,Math.abs(xx-x),Math.abs(yy-y)};
+    }
+
